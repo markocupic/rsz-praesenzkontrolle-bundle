d35667614b9eaa24c814e84a1d6d5ed0adaad5a2
Marko Cupic <m.cupic@gmx.ch>
2022-07-18 11:45:22 +0200

    Revert "Add and run ecs"
    This reverts commit 10b31db43409602a93fafe86d5617a38d7e22f07.

--- src/Excel/RszPraesenzkontrolleDownload.php
+++ src/Excel/RszPraesenzkontrolleDownload.php
@@ -1,23 +1,15 @@
 <?php
 
 /**
- * @copyright  Marko Cupic 2022 <m.cupic@gmx.ch>
+ * @copyright  Marko Cupic 2020 <m.cupic@gmx.ch>
+ * @author     Marko Cupic
+ * @package    RSZ Benutzerverwaltung
  * @license    MIT
- *
  * @see        https://github.com/markocupic/rsz-benutzerverwaltung-bundle
+ *
  */
 
 declare(strict_types=1);
-
-/*
- * This file is part of RSZ Präsenzkontrolle Bundle.
- *
- * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
- * @license MIT
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/markocupic/rsz-praesenzkontrolle-bundle
- */
 
 namespace Markocupic\RszPraesenzkontrolleBundle\Excel;
 
@@ -26,16 +18,25 @@ use Contao\Database;
 use Contao\Date;
 use Contao\Input;
 use Contao\StringUtil;
-use JetBrains\PhpStorm\NoReturn;
-use PhpOffice\PhpSpreadsheet\Exception;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+/**
+ * Class RszPraesenzkontrolleDownload
+ * @package Markocupic\RszBenutzerverwaltungBundle\Excel
+ */
 class RszPraesenzkontrolleDownload
 {
-    private ContaoFramework $framework;
-    protected array $opt;
+    /** @var ContaoFramework */
+    private $framework;
 
+    /** @var array */
+    protected $opt;
+
+    /**
+     * RszPraesenzkontrolleDownload constructor.
+     * @param ContaoFramework $framework
+     */
     public function __construct(ContaoFramework $framework)
     {
         $this->framework = $framework;
@@ -45,43 +46,49 @@ class RszPraesenzkontrolleDownload
     }
 
     /**
-     * @throws Exception
+     * @param array $opt
+     * @return Response
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
      */
-    #[NoReturn]
- public function excelExport(): void
- {
-     /** @var Date $dateAdapter */
-     $dateAdapter = $this->framework->getAdapter(Date::class);
+    public function excelExport(array $opt = [])
+    {
+        /** @var Date $dateAdapter */
+        $dateAdapter = $this->framework->getAdapter(Date::class);
 
-     // Get data
-     $arrData = $this->prepareData();
+        // Get data
+        $arrData = $this->prepareData();
 
-     $spreadsheet = new Spreadsheet();
-     $spreadsheet->getActiveSheet()->setTitle('Präsenzkontrolle '.$dateAdapter->parse('Y'));
-     $spreadsheet->setActiveSheetIndex(0);
+        $spreadsheet = new Spreadsheet();
+        $spreadsheet->getActiveSheet()->setTitle("Präsenzkontrolle " . $dateAdapter->parse("Y"));
+        $spreadsheet->setActiveSheetIndex(0);
 
-     foreach ($arrData as $intRow => $arrRow) {
-         foreach ($arrRow as $intColumn => $strValue) {
-             $spreadsheet->getActiveSheet()->setCellValueByColumnAndRow($intColumn + 1, $intRow + 1, $strValue);
-         }
-     }
+        foreach ($arrData as $intRow => $arrRow)
+        {
+            foreach ($arrRow as $intColumn => $strValue)
+            {
+                $spreadsheet->getActiveSheet()->setCellValueByColumnAndRow($intColumn + 1, $intRow + 1, $strValue);
+            }
+        }
 
-     // Set Text Rotation to top row
-     $spreadsheet->getActiveSheet()->getStyle('A1:ZZ1')->getAlignment()->setTextRotation(90);
+        // Set Text Rotation to top row
+        $spreadsheet->getActiveSheet()->getStyle("A1:ZZ1")->getAlignment()->setTextRotation(90);
 
-     // Set height of top row
-     $spreadsheet->getActiveSheet()->getRowDimension(1)->setRowHeight(75);
+        // Set height of top row
+        $spreadsheet->getActiveSheet()->getRowDimension(1)->setRowHeight(75);
 
-     // Send file to browser
-     $objWriter = new Xlsx($spreadsheet);
-     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-     header('Content-Disposition: attachment; filename="praesenzkontrolle_rsz_'.$dateAdapter->parse('Y-m-d').'.xlsx"');
-     header('Cache-Control: max-age=0');
-     $objWriter->save('php://output');
-     exit;
- }
+        // Send file to browser
+        $objWriter = new Xlsx($spreadsheet);
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header("Content-Disposition: attachment; filename=\"praesenzkontrolle_rsz_" . $dateAdapter->parse("Y-m-d") . ".xlsx\"");
+        header("Cache-Control: max-age=0");
+        $objWriter->save("php://output");
+        exit;
+    }
 
-    private function prepareData(): array
+    /**
+     * @return array
+     */
+    private function prepareData()
     {
         /** @var Database $databaseAdapter */
         $databaseAdapter = $this->framework->getAdapter(Database::class);
@@ -95,17 +102,18 @@ class RszPraesenzkontrolleDownload
         // Trainer Datenarray erstellen
         $data_arr_trainer = [];
         $db = $databaseAdapter->getInstance()->prepare('SELECT id, name FROM tl_user WHERE funktion LIKE ? ORDER BY name ASC')->execute('%Trainer%');
-
-        while ($db->next()) {
+        while ($db->next())
+        {
             $data_arr_trainer[$db->id]['name'] = $db->name;
             $data_arr_trainer[$db->id]['funktion'] = 'Trainer';
         }
 
         // Athleten Datenarray erstellen
         $data_arr_athlete = [];
-        $db = $databaseAdapter->getInstance()->prepare('SELECT id, name FROM tl_user WHERE funktion LIKE ? ORDER BY '.$this->opt['sortingDirection'])->execute('%Athlet%');
+        $db = $databaseAdapter->getInstance()->prepare("SELECT id, name FROM tl_user WHERE funktion LIKE ? ORDER BY " . $this->opt['sortingDirection'])->execute('%Athlet%');
 
-        while ($db->next()) {
+        while ($db->next())
+        {
             $data_arr_athlete[$db->id]['name'] = $db->name;
             $data_arr_athlete[$db->id]['funktion'] = 'Athlet';
         }
@@ -113,14 +121,18 @@ class RszPraesenzkontrolleDownload
         // Hier wird geprüft, ob der Trainer am Anlass anwesend war oder nicht
         // und dann werden die entsprechenden Werte ins dataArr geschrieben
         $db = $databaseAdapter->getInstance()->execute('SELECT * FROM tl_rsz_praesenzkontrolle ORDER BY start_date');
-
-        while ($db->next()) {
+        while ($db->next())
+        {
             $trainer_arr = $stringUtilAdapter->deserialize($db->trainers, true);
 
-            foreach (array_keys($data_arr_trainer) as $key) {
-                if (\in_array($key, $trainer_arr, true)) {
+            foreach ($data_arr_trainer as $key => $username)
+            {
+                if (in_array($key, $trainer_arr))
+                {
                     $data_arr_trainer[$key][$db->pid] = ['hours' => $db->hours, 'start_date' => $db->start_date, 'event' => $db->event];
-                } else {
+                }
+                else
+                {
                     $data_arr_trainer[$key][$db->pid] = ['hours' => '', 'start_date' => $db->start_date, 'event' => $db->event];
                 }
             }
@@ -129,10 +141,14 @@ class RszPraesenzkontrolleDownload
             // und dann werden die entsprechenden Werte ins dataArr geschrieben
             $athl_arr = $stringUtilAdapter->deserialize($db->athletes, true);
 
-            foreach (array_keys($data_arr_athlete) as $key) {
-                if (\in_array($key, $athl_arr, true)) {
+            foreach ($data_arr_athlete as $key => $username)
+            {
+                if (in_array($key, $athl_arr))
+                {
                     $data_arr_athlete[$key][$db->pid] = ['hours' => $db->hours, 'start_date' => $db->start_date, 'event' => $db->event];
-                } else {
+                }
+                else
+                {
                     $data_arr_athlete[$key][$db->pid] = ['hours' => '', 'start_date' => $db->start_date, 'event' => $db->event];
                 }
             }
@@ -144,8 +160,8 @@ class RszPraesenzkontrolleDownload
         $dateArr = [];
         $eventId = [];
         $eventComments = [];
-
-        while ($db->next()) {
+        while ($db->next())
+        {
             $dateArr[] = $db->start_date;
             $eventId[] = $db->pid;
             $eventComments[] = $db->comment;
@@ -153,8 +169,8 @@ class RszPraesenzkontrolleDownload
 
         // Hilfsarrays mit den Eventdaten
         $eventArr = [];
-
-        foreach ($eventId as $id) {
+        foreach ($eventId as $id)
+        {
             $db = $databaseAdapter->getInstance()->prepare('SELECT art FROM tl_rsz_jahresprogramm WHERE id=?')->execute($id);
             $eventArr[] = ['art' => $db->art];
         }
@@ -165,8 +181,8 @@ class RszPraesenzkontrolleDownload
         $arrRow = [];
         // Leerzelle
         $arrRow[] = '';
-
-        foreach ($dateArr as $date) {
+        foreach ($dateArr as $date)
+        {
             $arrRow[] = $dateAdapter->parse('Y-m-d', $date);
         }
         $arrRows[] = $arrRow;
@@ -175,8 +191,8 @@ class RszPraesenzkontrolleDownload
         $arrRow = [];
         // Leerzelle
         $arrRow[] = '';
-
-        foreach ($eventArr as $event) {
+        foreach ($eventArr as $event)
+        {
             $arrRow[] = $event['art'];
         }
         $arrRows[] = $arrRow;
@@ -187,11 +203,12 @@ class RszPraesenzkontrolleDownload
         // Eine Leerzeile mit Übertitel "Trainer"
         $arrRows[] = ['Trainer'];
         // Zeilen mit den Trainern
-        foreach (array_keys($data_arr_trainer) as $key) {
+        foreach ($data_arr_trainer as $key => $userArr)
+        {
             $arrRow = [];
             $arrRow[] = $data_arr_trainer[$key]['name'];
-
-            foreach ($eventId as $eventPid) {
+            foreach ($eventId as $eventPid)
+            {
                 $arrRow[] = $data_arr_trainer[$key][$eventPid]['hours'];
             }
             $arrRows[] = $arrRow;
@@ -204,11 +221,12 @@ class RszPraesenzkontrolleDownload
         $arrRows[] = ['Athleten'];
 
         // Datenzeilen mit den Athleten
-        foreach (array_keys($data_arr_athlete) as $key) {
+        foreach ($data_arr_athlete as $key => $userArr)
+        {
             $arrRow = [];
             $arrRow[] = $data_arr_athlete[$key]['name'];
-
-            foreach ($eventId as $eventPid) {
+            foreach ($eventId as $eventPid)
+            {
                 $arrRow[] = $data_arr_athlete[$key][$eventPid]['hours'];
             }
             $arrRows[] = $arrRow;
@@ -220,12 +238,13 @@ class RszPraesenzkontrolleDownload
         // Zeile mit Bemerkungen
         $arrRow = [];
         $arrRow[] = 'Bemerkungen';
-
-        foreach ($eventComments as $eventComment) {
-            $arrRow[] = utf8_decode((string) Input::stripTags($eventComment));
+        foreach ($eventComments as $eventComment)
+        {
+            $arrRow[] = utf8_decode((string)Input::stripTags($eventComment));
         }
         $arrRows[] = $arrRow;
 
         return $arrRows;
     }
+
 }
